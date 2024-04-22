d1721ebb6cad38ba0aee92e8a5bab73d967dc3dd
Arsha <160167081+arshasuresh03@users.noreply.github.com>
2024-04-22 23:40:36 +0530

    Floyd.cpp

---
+++ Floyd.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define INF 99999
+#define V 4
+
+void printSolution(const vector<vector<int>>& dist) {
+    cout << "The following matrix shows the shortest distances between every pair of vertices:\n";
+    for (int i = 0; i < V; ++i) {
+        for (int j = 0; j < V; ++j) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << "   ";
+        }
+        cout << endl;
+    }
+}
+
+void floydWarshall(vector<vector<int>>& graph) {
+    vector<vector<int>> dist(graph);
+
+    for (int k = 0; k < V; ++k) {
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < V; ++j) {
+                if (dist[i][k] != INF && dist[k][j] != INF && dist[i][j] > dist[i][k] + dist[k][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    printSolution(dist);
+}
+
+int main() {
+    vector<vector<int>> graph = {
+        {0, 5, INF, 10},
+        {INF, 0, 3, INF},
+        {INF, INF, 0, 1},
+        {INF, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+
+    return 0;
+}
